0279e341a75d470e2cfb5922f9da4a622afd2ed3
sebpalmer@gmx.com <sebpalmer@gmx.com>
2015-08-24 15:46:59 +0100

    home page works minimally

--- functional_tests.py
+++ functional_tests.py
@@ -11,12 +11,7 @@ class NewVisitorTest(unittest.TestCase):
 
 	def test_can_start_a_list_and_retrieve_it_later(self) :
 		self.browser.get('http://localhost:8000')
-		self.assertIn('To-Do', self.browser.title)
-		
-
-		
-		
-		browser.quit()
+		self.assertIn('To-do', self.browser.title)
 	
 if __name__=='__main__':
 	unittest.main(warnings='ignore')
--- lists/tests.py
+++ lists/tests.py
@@ -1,9 +1,17 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
+from django.http import HttpRequest
 from lists.views import home_page
 
 # Create your tests here.
-class SmokeTest(TestCase):
+class HomePageTest(TestCase):
 	def test_root_url_resolves_to_home_page_view(self):
 		found = resolve('/')
 		self.assertEqual(found.func, home_page)
+		
+	def test_home_page_has_proper_html(self):
+		request=HttpRequest()
+		response=home_page(request)
+		self.assertTrue(response.content.startswith(b'<html>'))
+		self.assertTrue(response.content.endswith(b'</html>'))
+		self.assertIn(b'<title>To-do lists</title>', response.content)
