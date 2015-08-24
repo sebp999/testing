43dbc4b004b8d82d5426ab6b72b9b514f196b4f5
sebpalmer@gmx.com <sebpalmer@gmx.com>
2015-08-24 15:17:21 +0100

    Test reorganised

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,22 @@
 from selenium import webdriver
+import unittest
 
-brower=webdriver.Firefox()
-brower.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in brower.title
+	def setUp(self):
+		self.browser=webdriver.Firefox()
+		
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_can_start_a_list_and_retrieve_it_later(self) :
+		self.browser.get('http://localhost:8000')
+		self.assertIn('To-Do', self.browser.title)
+		
+
+		
+		
+		browser.quit()
+	
+if __name__=='__main__':
+	unittest.main(warnings='ignore')
