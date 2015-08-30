e4a60f528615bd20bdc97ad254ba9e6a68b1230f
unknown <sebpalmer@gmx.com>
2015-08-30 19:17:23 +0100

    added lists.  each post is a new list though.

--- lists/tests.py
+++ lists/tests.py
@@ -3,19 +3,27 @@ from django.core.urlresolvers import resolve
 from django.http import HttpRequest
 from lists.views import home_page, view_list
 from django.template.loader import render_to_string
-from lists.models import Item
+from lists.models import Item, List
 
 # Create your tests here.
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
 	def test_saving_and_retrieving_items(self):
+		alist=List()
+		alist.save()
+		
 		first_item=Item()
 		first_item.text='The first item'
+		first_item.list=alist
 		first_item.save()
 		
 		second_item=Item()
 		second_item.text='The second item'
+		second_item.list=alist
 		second_item.save()
+		
+		saved_list=List.objects.first()
+		self.assertEqual(saved_list, alist)
 		
 		saved_items = Item.objects.all()
 		self.assertEqual(saved_items.count(), 2)
@@ -24,8 +32,10 @@ class ItemModelTest(TestCase):
 		second_saved_item = saved_items[1]
 		
 		self.assertEqual(first_saved_item.text, 'The first item')
+		self.assertEqual(first_saved_item.list, alist)
+		
 		self.assertEqual(second_saved_item.text, 'The second item')
-
+		self.assertEqual(second_saved_item.list, alist)
 
 
 class HomePageTest(TestCase):
@@ -54,8 +64,9 @@ class NewListTest(TestCase):
 
 class ListViewTest(TestCase):
 	def test_displays_all_items(self):
-		Item.objects.create(text='item1')
-		Item.objects.create(text='item2')
+		a_list = List.objects.create()
+		Item.objects.create(text='item1', list=a_list)
+		Item.objects.create(text='item2', list=a_list)
 		request=HttpRequest()
 		response=view_list(request)
 		self.assertIn('item1',response.content.decode())
