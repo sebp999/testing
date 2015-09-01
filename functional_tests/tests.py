4321e2a0e93188bb1929ba70469c8dc150443adc
unknown <sebpalmer@gmx.com>
2015-09-01 14:31:35 +0100

    move static and database outside of main source

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,23 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+import sys
 import unittest
 
 class NewVisitorTest(StaticLiveServerTestCase):
+	@classmethod
+	def setUpClass(cls):
+		for arg in sys.argv:
+			if 'liveserver' in arg:
+				cls.server_url =  'http://'+arg.split('=')[1]
+				return
+		super().setUpClass()
+		cls.server_url = cls.live_server_url
+
+	@classmethod
+	def tearDownClass(cls):
+		if cls.server_url == cls.live_server_url:
+			super().tearDownClass()
 
 	def setUp(self):
 		self.browser=webdriver.Firefox()
@@ -18,7 +32,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		self.assertIn(text,[row.text for row in rows])
 		
 	def test_can_start_a_list_and_retrieve_it_later(self) :
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		self.assertIn('To-do', self.browser.title)
 		inputbox=self.browser.find_element_by_id('id_new_item')
 		header_text=self.browser.find_element_by_tag_name('h1').text
@@ -56,7 +70,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		self.browser=webdriver.Firefox()
 		
 		#go home page.  no sign of previous list
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		page_text=self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('peacock',page_text)
 		
@@ -92,7 +106,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		self.assertNotIn('peacock',page_text)
 
 	def test_layout_and_styling(self):
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		self.browser.set_window_size(1024,768)
 		inputbox=self.browser.find_element_by_id('id_new_item')
 		self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width']/2, 512, delta=20)
