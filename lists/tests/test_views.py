d7b537fc0a6c4111262bcd83777cabb18991b643
unknown <sebpalmer@gmx.com>
2015-09-10 09:42:19 +0100

    Refactored got rid of new_item which is now handled by view_list

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -28,7 +28,6 @@ class NewListTest(TestCase):
 		new_item=Item.objects.first()
 		self.assertEqual(new_item.text, 'A new list item')
 
-		
 	def test_redirect_after_post(self):
 		response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
 		new_list=List.objects.first()
@@ -82,14 +81,11 @@ class ListViewTest(TestCase):
 		
 		self.assertEqual(response.context['list'],a_list)
 		
-	
-		
-class NewItemTest(TestCase):
 	def test_can_save_post_to_an_existing_list(self):
 		other_list=List.objects.create()
 		correct_list=List.objects.create()
 		
-		self.client.post('/lists/%d/add_item' % (correct_list.id), data={'item_text':'A new item for existing list'})
+		self.client.post('/lists/%d/' % (correct_list.id), data={'item_text':'A new item for existing list'})
 		self.assertEqual(Item.objects.count(), 1)
 		new_item=Item.objects.first()
 		self.assertEqual(new_item.text, 'A new item for existing list')
@@ -99,5 +95,7 @@ class NewItemTest(TestCase):
 		other_list=List.objects.create()
 		correct_list=List.objects.create()
 		
-		response = self.client.post('/lists/%d/add_item' % (correct_list.id), data={'item_text':'A new item for existing list'})
+		response = self.client.post('/lists/%d/' % (correct_list.id), data={'item_text':'A new item for existing list'})
 		self.assertRedirects(response, '/lists/%d/' % (correct_list.id))
+		
+
--- lists/views.py
+++ lists/views.py
@@ -18,13 +18,10 @@ def new_list(request):
 	return redirect('/lists/%d/' % (list_.id,))
 
 
-def add_item(request, list_id):
-	list_ = List.objects.get(id=list_id)
-	Item.objects.create(text=request.POST['item_text'], list=list_)
-	return redirect('/lists/%d/' % (list_.id,))
-
-
 def view_list(request, list_id):
 	list_ = List.objects.get(id=list_id)
-	return render(request, 'list.html', {'list': list_})
+	if request.method=='POST':
+		Item.objects.create(text=request.POST['item_text'], list=list_)
+		return redirect('/lists/%d/' % (list_.id,))
+	return render(request, 'list.html', {'list': list_}) 
 
